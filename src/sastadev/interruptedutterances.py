b33b2e205386ab9e9d8c449f4ebf05ca254be684
Odijk <j.odijk@uu.nl>
2026-02-02 10:16:10 +0100

    adapted interrutedutterances.py

--- src/sastadev/interruptedutterances.py
+++ src/sastadev/interruptedutterances.py
@@ -9,10 +9,11 @@ After the application of the pre and core queries, the results obtained are modi
 belonging to the interrupted utterance are associated with the id of that utterances,
 and the annotations belonging to the continuation utterances are associated with the id of that utterance.
 """
-
+from collections import defaultdict
+from sastadev.allresults import AllResults
 from sastadev.conf import settings
-from sastadev.sastatypes import ExactResults, SynTree
-from sastadev.treebankfunctions import getorigutt, getsentence
+from sastadev.sastatypes import ExactResults, ExactResultsDict, SynTree
+from sastadev.treebankfunctions import getnodeyield, getorigutt, getsentence, getxsid
 from sastadev.cleanCHILDEStokens import cleantext
 
 def getcombinationtree(interrupted_tree, continuation_tree) -> SynTree:
@@ -26,14 +27,54 @@ def getcombinationtree(interrupted_tree, continuation_tree) -> SynTree:
     newtree = settings.PARSE(newcleanutt)
     return newtree
 
-def adaptresults(exactresults: ExactResults, newtree: SynTree, interrupted_tree: SynTree) -> ExactResults:
+def adaptresults(allresults: AllResults,  newtree: SynTree, interrupted_tree: SynTree) -> AllResults:
     # remove all results for the interrupted_tree
     # replace the interrupted_tree with the newtree
     # determine the length of the interrupted utterance and find the associated end
-    # add all results for positions <= interrupted_end to the interrupted xsid
-    # add allresults for positions > interrupted_end to the continuation xsid
+    # add all results for positions < interrupted_end to the interrupted xsid
+    # add allresults for positions >= interrupted_end to the continuation xsid
     # replace the interrupted_tree with the newtree
 
     # after this function: also adapt nodeendmap[uttid] for the interrupted xsid
     # think about what to do with allutts
+
+
+    exactresultsdict = allresults.exactresults
+    interrupted_tree_length = len(getnodeyield(interrupted_tree))
+    interrupted_tree_xsid = getxsid(interrupted_tree)
+    newtree_xsid = getxsid(newtree)
+    newexactresultsdict = defaultdict(list)
+    newreskeyresults = []
+    for reskey in exactresultsdict:
+        reskeyresults = exactresultsdict[reskey]
+
+        for xsid, pos in reskeyresults:
+            if xsid == newtree_xsid:
+                if pos >= interrupted_tree_length:
+                    newreskeyresults[xsid].append((xsid, pos))
+                else:
+                    newreskeyresults[interrupted_tree_xsid].append((xsid, pos))
+            else:
+                newreskeyresults.append((xsid, pos))
+        newexactresultsdict[reskey] = newreskeyresults
+
+    # also adapt allcoreresults
+    # adapt allmatches  # no this is still empty in this stage
+    # maybe adapt allutts
+    # adapt analysedtrees  # nee niet nodig want ze he
+    new_analysedtrees = []
+    for xsid, tree in allresults.analysedtrees:
+        if xsid == interrupted_tree_xsid:
+            newtuple = (xsid, newtree)
+        else:
+            newtuple = (xsid, tree)
+        new_analysedtrees.append(newtuple)
+
+
+    # or build a new allresults
+    allresults.exactresults =  newexactresultsdict
+    allresults.analysedtrees = new_analysedtrees
+    return allresults
+
+
     pass
